6ddb79a42c28ef6843004d4f02aab9cc2b2cf313
eklipsorz <eklipsorz@protonmail.ch>
2022-09-30 02:28:08 +0800

    feat: add bump animation to button

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,39 @@
 import styles from './CartButton.module.css';
 import CartIcon from './CartIcon';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import CartContext from '../../store/cart-context';
 
 const CartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const cartBtnClasses = `${styles['cart-button']} ${
+    btnIsHighlighted ? styles['bump'] : ''
+  }`;
+
+  useEffect(() => {
+    if (!items.length) {
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+
+    const identifier = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [items]);
+
   return (
-    <button className={styles['cart-button']} onClick={props.onClick}>
+    <button className={cartBtnClasses} onClick={props.onClick}>
       <span className={styles['cart-icon']}>
         <CartIcon />
       </span>
