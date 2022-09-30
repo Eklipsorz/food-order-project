a4a8096016a2617cacb545d8c3fcc05a11968560
eklipsorz <eklipsorz@protonmail.ch>
2022-09-30 23:02:00 +0800

    finish add question

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,39 +1,18 @@
 import styles from './CartButton.module.css';
 import CartIcon from './CartIcon';
-import { useContext, useState, useEffect } from 'react';
+import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
 
 const CartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
   const { items } = cartCtx;
   const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
-  const cartBtnClasses = `${styles['cart-button']} ${
-    btnIsHighlighted ? styles['bump'] : ''
-  }`;
-
-  useEffect(() => {
-    if (!items.length) {
-      return;
-    }
-
-    setBtnIsHighlighted(true);
-
-    const identifier = setTimeout(() => {
-      setBtnIsHighlighted(false);
-    }, 300);
-
-    return () => {
-      clearTimeout(identifier);
-    };
-  }, [items]);
-
   return (
-    <button className={cartBtnClasses} onClick={props.onClick}>
+    <button className={styles['cart-button']} onClick={props.onClick}>
       <span className={styles['cart-icon']}>
         <CartIcon />
       </span>
